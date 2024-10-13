1e7a71da09b042ffb0e917a443c051ccc810fc8c
jana1454 <jahmed1454@gmail.com>
2024-10-13 06:00:57 -0400

    2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,12 @@ export default function Hero() {
     return (
         <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4'>
             <div className='flex flex-col gap-4'>
+            <i className="fa-solid fa-dumbbell fa-bounce fa-5x"></i>
 
                 <p>IT'S TIME TO GET A</p>
-                <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Cyber<span className='text-blue-400'>pump</span></h1>
+                <h1 className='uppercase font-semibold text-5xl sm:text-5xl md:text-7xl lg:text-8xl'>Cyber<span className='text-purple-300'>pump</span></h1>
             </div>
-            <p className='text-sm md:text-base font-light'>Are you ready to be fully immersed in the <span className='text-blue-400 font-medium'>future of fitness</span> and get ready to get that <span className='text-blue-400 font-medium'>CYBER PUMP!</span></p>
+            <p className='text-sm md:text-base font-light'>Are you ready to be fully immersed in the <span className='text-[#ff007f] font-medium'>future of fitness</span> and get ready to get that <span className='text-[#ff007f] font-medium'>CYBER PUMP!</span></p>
             <Button func={() => {
                 window.location.href = '#generate'
             }} text={"Accept & Begin"}></Button>
